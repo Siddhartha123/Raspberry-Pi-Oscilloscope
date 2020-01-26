e5231b7100ca52014f70bacd70ebe9aa9a72cb09
Ippili Sidhartha Kumar <iskumar789@gmail.com>
2020-01-26 10:42:43 +0530

    simulation of sine plot using OpenCV

--- simulation codes/screen.cpp
+++ simulation codes/screen.cpp
@@ -1,4 +1,6 @@
 #include "screen.h"
+#include "plot.h"
+
 using namespace std;
 
 /*
@@ -6,15 +8,53 @@ void CursorMove (unsigned nRow, unsigned nColumn);
 TScreenColor
 void Rotor (unsigned nIndex, unsigned nCount);
 */
-int main(){
-    Screen m_screen(640,480);
-    //m_screen.display();
-    m_screen.SetPixel(0,100);
-    m_screen.SetPixel(100,100);
-    m_screen.SetPixel(100,0);
-    m_screen.SetPixel(0,0);
-    
-    m_screen.display();
-    return 0;
+int main()
+{
 
+    double t = 0;
+    Screen m_screen(640, 480);
+    int i;
+
+    //variables for adc data
+    long int idx = 0;
+    int oldest_idx = 0;
+    int samples[WINDOW_SIZE];
+    int last_sum, data, current_sum;
+    m_screen.display();
+
+    while (!m_screen.finish)
+    {
+        //do stuff here
+        // t++;
+        // if ((int)t % 640 == 0)
+        //     m_screen.clear();
+        // m_screen.SetPixel((int)t % 640, (int)(240 + 100 * sin(0.02 * t)));
+
+        m_screen.clear();
+
+        scan(&m_screen, idx, oldest_idx, samples);
+
+        m_screen.update();
+        last_sum = 0;
+        for (i = 0; i < WINDOW_SIZE; i++)
+            last_sum += samples[i];
+
+        data = read_adc(idx, oldest_idx, samples);
+
+        current_sum = 0;
+        for (i = 0; i < WINDOW_SIZE; i++)
+            current_sum += samples[i];
+        while (!(int(abs(data - 240)) == 2 && current_sum > last_sum))
+        {
+            last_sum = 0;
+            for (i = 0; i < WINDOW_SIZE; i++)
+                last_sum += samples[i];
+            data = read_adc(idx, oldest_idx, samples);
+            current_sum = 0;
+            for (i = 0; i < WINDOW_SIZE; i++)
+                current_sum += samples[i];
+        }
+        usleep(100000);
+    }
+    return 0;
 }
--- simulation codes/screen.h
+++ simulation codes/screen.h
@@ -2,37 +2,65 @@
 #define _screen_h
 
 #include <bits/stdc++.h>
+#include <unistd.h>
 #include <opencv/cv.h>
 #include <opencv/cv.hpp>
 #include "opencv2/core/core.hpp"
 #include <opencv/highgui.h>
+
 using namespace cv;
 using namespace std;
+
+struct BGR
+{
+    uchar blue;
+    uchar green;
+    uchar red;
+};
+
 class Screen
 {
     int height;
     int width;
     Mat pImgData;
 
-  public:
+public:
+    bool finish;
     Screen(int h, int w)
     {
         height = h;
         width = w;
+        finish = false;
         pImgData = Mat::zeros(w, h, CV_8UC3);
     }
 
     bool SetPixel(int x, int y, int COLOR = 0)
     {
-        pImgData.at<Vec3b>(y, x) = 255, 0, 0;
+        pImgData.at<Vec3b>(y, x) = {0, 255, 255};
         return true;
+    }
+
+    Scalar getPixel(int x, int y)
+    {
+        return pImgData.at<Vec3b>(y, x);
     }
 
     void display()
     {
         namedWindow("screen", CV_WINDOW_AUTOSIZE);
         imshow("screen", pImgData);
-        waitKey(0);
+    }
+
+    void clear()
+    {
+        pImgData = Mat::zeros(width, height, CV_8UC3);
+    }
+
+    bool update()
+    {
+        imshow("screen", pImgData);
+        if (cv::waitKey(50) == 27)
+            finish = true;
     }
 };
 
